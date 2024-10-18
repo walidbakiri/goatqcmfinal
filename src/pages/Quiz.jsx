29a8099c4ee62fbe42798ee45048fdecb8c9450e
unknown <bakiri.w@gmail.com>
2024-10-18 15:26:55 +0100

    updated test btn

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -829,7 +829,6 @@ function Quiz() {
             className={classes.contanerspace}
             data-theme={isDark ? "dark" : "light"}
           >
-            <button onClick={handleShowCours}>test</button>
             <div className={classes.allcards}>
               <div className={`${classes.qcmmodele} table-hover shadow`}>
                 <div
